c80a97a5550704715fa35d2956db6010f25ee366
ologinov <olognv@gmail.com>
2017-10-22 19:22:02 +0300

    1

--- OLOG/CoreUI/LayoutCoreUI.php
+++ OLOG/CoreUI/LayoutCoreUI.php
@@ -61,7 +61,7 @@ class LayoutCoreUI implements
 
     <!-- Bootstrap and necessary plugins -->
     <!--<script src="js/libs/jquery.min.js"></script>-->
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
     <!--<script src="js/libs/tether.min.js"></script>-->
     <script src="/assets/bootstrap4/assets/js/vendor/popper.min.js"></script>
     <script src="/assets/bootstrap4/js/bootstrap.js"></script>
